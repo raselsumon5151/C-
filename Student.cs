f5d1a91f1756451fbcf7a89cfc9e2119252d8667
Rasel Sumon <raselsumon51@gmail.com>
2020-03-02 16:29:46 +0600

    it's a comment

---
+++ Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceExamp
+{
+    class Student : Person
+    {
+        public string Program;
+        public int Year;
+        public double Fee;
+       
+        public Student(string program, int year, double fee,string Name,string Address) : base( Name, Address)
+        {
+            this.Program =program;
+            this.Year = year;
+            this.Fee = fee;
+       }
+        
+       public override void GetDetails() 
+        {
+            Console.WriteLine($"Person name :{Name} Address:{Address} Program:{Program} Year:{Year} Fee :{Fee}");
+        }
+
+    }
+}
